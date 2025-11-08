349f5a118632780ac02b7890d22690c89af55f3b
Thrijwal K <thrijwalk@gmail.com>
2025-11-08 03:27:24 +0000

    Update metadata for the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Thrijwal',
+  description: '.',
+  generator: 'Thrijwal',
   icons: {
     icon: [
       {
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '',
   },
 }
 
